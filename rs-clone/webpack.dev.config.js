8903f7b481a2e30db4560073428ac70590324461
Mikhail Sprygin <wave103x@gmail.com>
2023-01-30 13:21:24 +0300

    feat: add base configs

---
+++ rs-clone/webpack.dev.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "./src/index.ts"),
+  },
+  mode: "development",
+  target: "web",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    port: 8080,
+    hot: true,
+    static: path.resolve(__dirname, "./dist"),
+  },
+};
