11350734d8921405a3a1bb85fee5f0ea92f9185d
Mikhail Sprygin <wave103x@gmail.com>
2023-02-07 13:51:08 +0300

    feat: add page heading api to Header, add rem breakpoints on 768, 576px

--- rs-clone/src/components/Views/enums/app-css-class.ts
+++ rs-clone/src/components/Views/enums/app-css-class.ts
@@ -10,6 +10,7 @@ const enum AppCssClass {
   ACC_MENU_HIDDEN = 'acc-menu_hidden',
   ACC_MENU_NAME = 'acc-menu__login-name',
   ACC_MENU_BTN = 'acc-menu__button',
+  PAGE_HEADING = 'page-heading',
 
   FIRST_VIEW_LOGO = 'first-view__logo',
   FIRST_VIEW_BUTTONS = 'fist-view__buttons',
